5a6f0bf1ba843f3519e888db54047cb419ff7cdb
AmirHossein137 <amirhossein.hossseiini1372@gmail.com>
2024-09-05 07:43:43 -0700

    u

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,27 +1,32 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from 'axios'
+import axios from "axios";
 import ProductCard from "./ProductCard";
 
 const Home = () => {
-  const { data , isLoading } = useQuery({
+  const { data } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      return axios.get('https://api.escuelajs.co/api/v1/products')
+      return axios.get("https://api.escuelajs.co/api/v1/products");
     },
-
   });
 
-  console.log(data.data);
+  console.log(data)
 
   return (
     <>
-    <div className="grid grid-cols-5 gap-3">
-      {data?.data?.map((item) => (
-        <ProductCard key={item.id} item={item} />
-      ))}
-    </div>
+      <div className="grid grid-cols-5 gap-3">
+        {data?.data.map((item) => (
+          <ProductCard
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          images={item.images}
+          price={item.price}
+        />
+        ))}
+      </div>
     </>
-  )
+  );
 };
 
 export default Home;
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,13 @@
-
-const ProductCard = () => {
+const ProductCard = ({ key, title, description, images, price }) => {
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div key={key} className="shadow p-2 border rounded-lg border-gray-200">
+      <div>
+        {images.map((i) => (
+          <img src={i.substring(2, i.length - 2)} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
